6e1338b2ba4e027434d05b5fd04631d20ab4472e
stbrumm <thoser666@web.de>
2026-04-28 18:36:35 +0200

    adding jakarta.websocket

--- src/main/java/com/jclaw/jclaw/JClawApplication.java
+++ src/main/java/com/jclaw/jclaw/JClawApplication.java
@@ -1,7 +1,9 @@
 package com.jclaw.jclaw;
 
+import org.springframework.ai.autoconfigure.ollama.OllamaConnectionDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JClawApplication {
@@ -10,4 +12,18 @@ public class JClawApplication {
         SpringApplication.run(JClawApplication.class, args);
     }
 
+    /**
+     * Workaround for Spring AI 1.0.0-M1 AOT issue.
+     * Manually defining OllamaConnectionDetails prevents the AOT processor from 
+     * failing on the private PropertiesOllamaConnectionDetails inner class.
+     */
+    @Bean
+    public OllamaConnectionDetails ollamaConnectionDetails() {
+        return new OllamaConnectionDetails() {
+            @Override
+            public String getBaseUrl() {
+                return "http://localhost:11434";
+            }
+        };
+    }
 }
